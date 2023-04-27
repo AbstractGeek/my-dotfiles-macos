431a913123942cdeeb0f7c872150f69e0a4dd88a
Dinesh Natesan <AbstractGeek@users.noreply.github.com>
2023-04-26 22:29:44 -0700

    added obsidian and spyglass config files

--- config/fish/conf.d/virtualfish-loader.fish
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -3,8 +3,8 @@ set -g PROJECT_HOME /Users/dinesh/Codebank
 
 set -g VIRTUALFISH_VERSION 2.5.4
 set -g VIRTUALFISH_PYTHON_EXEC /Users/dinesh/.local/pipx/venvs/virtualfish/bin/python
-source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/virtual.fish
-source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/compat_aliases.fish
-source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/projects.fish
-source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/environment.fish
+source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/virtual.fish
+source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/compat_aliases.fish
+source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/projects.fish
+source /Users/dinesh/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/environment.fish
 emit virtualfish_did_setup_plugins
